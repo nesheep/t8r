adbce3923747caff285a1158a203dbe3a3adc967
horikawa <nesheep.dream@gmail.com>
2023-01-21 12:42:53 +0900

    Add cat command

--- cmd/t8r/main.go
+++ cmd/t8r/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"strings"
 
 	"github.com/nesheep/t8r"
@@ -12,12 +13,30 @@ func main() {
 		Use:   "echo [string to echo]",
 		Short: "Echo anything to the screen",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			t8r.Println(strings.Join(args, " "), nil)
-		},
+		Run:   echo,
+	}
+
+	catCmd := &cobra.Command{
+		Use:   "cat [filenaes]",
+		Short: "Print files to the screen",
+		Args:  cobra.MinimumNArgs(1),
+		Run:   cat,
 	}
 
 	rootCmd := &cobra.Command{Use: "t8r"}
-	rootCmd.AddCommand(echoCmd)
+	rootCmd.AddCommand(echoCmd, catCmd)
 	rootCmd.Execute()
+}
+
+func echo(cmd *cobra.Command, args []string) {
+	t8r.Println(strings.Join(args, " "), nil)
+}
+
+func cat(cmd *cobra.Command, args []string) {
+	for _, v := range args {
+		err := t8r.PrintFile(v, nil)
+		if err != nil {
+			log.Print(err)
+		}
+	}
 }
--- t8r.go
+++ t8r.go
@@ -1,29 +1,46 @@
 package t8r
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"time"
 )
 
 type Options struct{}
 
-func Indexes(s string, o *Options) []int {
-	res := make([]int, 0, len(s))
-	for i := range s {
-		res = append(res, i+1)
+func Slice(s string, o *Options) []string {
+	res := make([]string, 0, len(s))
+	for _, v := range s {
+		res = append(res, string(v))
 	}
 	return res
 }
 
 func Println(s string, o *Options) {
-	indexes := Indexes(s, o)
-	for i := range indexes {
-		start := 0
-		if i > 0 {
-			start = indexes[i-1]
-		}
-		fmt.Print(s[start:indexes[i]])
-		time.Sleep(200 * time.Millisecond)
+	sl := Slice(s, o)
+	for _, v := range sl {
+		fmt.Print(v)
+		time.Sleep(30 * time.Millisecond)
 	}
 	fmt.Println()
+}
+
+func PrintFile(filename string, o *Options) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		Println(line, o)
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
