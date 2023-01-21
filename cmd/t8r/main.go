5d47ceabbc5de5a26ce2a99212361df09b0db078
horikawa <nesheep.dream@gmail.com>
2023-01-21 10:37:36 +0900

    Cobra

--- cmd/t8r/main.go
+++ cmd/t8r/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"flag"
 	"strings"
 
 	"github.com/nesheep/t8r"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	flag.Parse()
-	args := flag.Args()
-	s := strings.Join(args, " ")
-	t8r.Println(s, nil)
+	echoCmd := &cobra.Command{
+		Use:   "echo [string to echo]",
+		Short: "Echo anything to the screen",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			t8r.Println(strings.Join(args, " "), nil)
+		},
+	}
+
+	rootCmd := &cobra.Command{Use: "t8r"}
+	rootCmd.AddCommand(echoCmd)
+	rootCmd.Execute()
 }
