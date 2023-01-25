f93749bf420b18de88e3609d847775b47aef8c9a
horikawa <nesheep.dream@gmail.com>
2023-01-25 18:52:52 +0900

    Update

--- writer.go
+++ writer.go
@@ -58,30 +58,34 @@ func (w Writer) writeHighlighted(p []byte) (int, error) {
 	}
 
 	for t := iter(); t != chroma.EOF; t = iter() {
-		e := w.Style.Get(t.Type)
-		if e.IsZero() {
-			w.write([]byte(t.Value))
-			continue
-		}
-
-		out := ""
-		if e.Bold == chroma.Yes {
-			out += "\033[1m"
-		}
-		if e.Underline == chroma.Yes {
-			out += "\033[4m"
-		}
-		if e.Italic == chroma.Yes {
-			out += "\033[3m"
-		}
-		if e.Colour.IsSet() {
-			out += fmt.Sprintf("\033[38;2;%d;%d;%dm", e.Colour.Red(), e.Colour.Green(), e.Colour.Blue())
-		}
-
-		fmt.Fprint(w.w, out)
-		w.write([]byte(t.Value))
-		fmt.Fprint(w.w, "\033[0m")
+		w.writeToken(t)
 	}
 
 	return len(p), nil
+}
+
+func (w Writer) writeToken(t chroma.Token) {
+	e := w.Style.Get(t.Type)
+	if e.IsZero() {
+		w.write([]byte(t.Value))
+		return
+	}
+
+	out := ""
+	if e.Bold == chroma.Yes {
+		out += "\033[1m"
+	}
+	if e.Underline == chroma.Yes {
+		out += "\033[4m"
+	}
+	if e.Italic == chroma.Yes {
+		out += "\033[3m"
+	}
+	if e.Colour.IsSet() {
+		out += fmt.Sprintf("\033[38;2;%d;%d;%dm", e.Colour.Red(), e.Colour.Green(), e.Colour.Blue())
+	}
+
+	fmt.Fprint(w.w, out)
+	w.write([]byte(t.Value))
+	fmt.Fprint(w.w, "\033[0m")
 }
